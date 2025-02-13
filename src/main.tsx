74eaf7f3917ee2ac1b5e2ee9d285efc863d72e10
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-13 10:29:12 -0400

    new - learning useParams and userSearchParams

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Menu } from './components/Menu'
+import { Post } from './components/Post'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <Menu />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/posts/:id' element={<Post />} />
+      <Route path='/posts' element={<Post />} />
+    </Routes>
+  </BrowserRouter>
 )
