cdc101ad79a95d6aef9152a5c9b484673fbbd40c
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-13 10:20:55 -0400

    new - creating first routes and using Link

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import { Home } from './components/Home'
+import { About } from './components/About'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Menu } from './components/Menu'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
